7e060c2e6e10c82e5bf5e9c29a709e92457df7d0
lisi <lisi@itcast.com>
2019-06-10 18:48:19 +0800

    修改工程又次添加

--- firstClass.java
+++ firstClass.java
@@ -2,16 +2,36 @@
 
 public class firstClass {
     public static void main(String[] args) {
-           System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
         //qwefghj
         //哈喽啊
     }
-    public void text(){
+
+    public void text() {
         System.out.println("asdfg");
         System.out.println("阿斯蒂芬规划局");
     }
