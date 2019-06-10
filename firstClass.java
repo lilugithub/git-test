b81ba71ad344f7a0413fae1c1ac4e2f18807b8bf
lisi <lisi@itcast.com>
2019-06-10 18:26:36 +0800

    修改工程又次添加

--- firstClass.java
+++ firstClass.java
@@ -9,8 +9,10 @@ public class firstClass {
         System.out.println("hello git");
         System.out.println("hello git");
         //qwefghj
+        //哈喽啊
     }
     public void text(){
         System.out.println("asdfg");
+        System.out.println("阿斯蒂芬规划局");
     }
 }
